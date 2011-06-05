aa9291623992e3003f4177e45a71cedf363947f2
John Gietzen <otac0n@gietzen.us>
2011-06-05 08:51:50 -0500

    Initial commit.

---
+++ MyLife/App.xaml.cs
@@ -0,0 +1,103 @@
+﻿// <copyright file="App.xaml.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the App class.
+        /// </summary>
+        public App()
+        {
+            // Global handler for uncaught exceptions. 
+            this.UnhandledException += this.Application_UnhandledException;
+
+            // Show graphics profiling information while debugging.
+            if (Debugger.IsAttached)
+            {
+                // Display the current frame rate counters.
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                // Show the areas of the app that are being redrawn in each frame.
+                ////Application.Current.Host.Settings.EnableRedrawRegions = true;
+
+                // Enable non-production analysis visualization mode, 
+                // which shows areas of a page that are being GPU accelerated with a colored overlay.
+                ////Application.Current.Host.Settings.EnableCacheVisualization = true;
+            }
+
+            // Standard Silverlight initialization
+            this.InitializeComponent();
+
+            // Phone-specific initialization
+            this.InitializePhoneApplication();
+        }
+
+        /// <summary>
+        /// Gets the root frame of the Phone Application.
+        /// </summary>
+        /// <returns>The root frame of the Phone Application.</returns>
+        public PhoneApplicationFrame RootFrame { get; private set; }
+
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+            // Code to execute when the application is launching (e.g., from Start)
+            // This code will not execute when the application is reactivated
+        }
+
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+            // Code to execute when the application is activated (brought to foreground)
+            // This code will not execute when the application is first launched
+        }
+
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+            // Code to execute when the application is deactivated (sent to background)
+            // This code will not execute when the application is closing
+        }
+
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+            // Code to execute when the application is closing (e.g., user hit Back)
+            // This code will not execute when the application is deactivated
+        }
+
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // A navigation has failed; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+    }
+}
---
+++ MyLife/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="MainPage.xaml.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using Microsoft.Phone.Controls;
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
