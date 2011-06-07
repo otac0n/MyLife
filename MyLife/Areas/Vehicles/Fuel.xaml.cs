9f240d371b2f2887ef6a9ba28f6edf361c653c4d
John Gietzen <otac0n@gietzen.us>
2011-06-06 21:40:12 -0500

    Added basic fuel page.

---
+++ MyLife/Areas/Vehicles/Fuel.xaml.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Fuel.xaml.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife.Areas.Vehicles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using Microsoft.Phone.Controls;
+
+    public partial class Fuel : PhoneApplicationPage
+    {
+        public Fuel()
+        {
+            InitializeComponent();
+        }
+    }
+}
