3092ec047ef20d727b015b91673184533d386da1
John Gietzen <otac0n@gietzen.us>
2011-06-06 23:31:58 -0500

    Fixed StyleCop violations for Maintenance.xaml.cs

--- MyLife/Areas/Vehicles/Maintenance.xaml.cs
+++ MyLife/Areas/Vehicles/Maintenance.xaml.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
+﻿// <copyright file="Maintenance.xaml.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
 
 namespace MyLife.Areas.Vehicles
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using Microsoft.Phone.Controls;
+
     public partial class Maintenance : PhoneApplicationPage
     {
         public Maintenance()
