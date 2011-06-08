394950e4d0828b642798f0e4e2072c4763e0dd6c
John Gietzen <otac0n@gietzen.us>
2011-06-07 22:24:27 -0500

    Added vehicle database customizations.

---
+++ MyLife/Areas/Vehicles/VehicleDatabase.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="VehicleDatabase.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Database
+    {
+        private List<FuelEntry> entries = new List<FuelEntry>();
+
+        public List<FuelEntry> Entries
+        {
+            get
+            {
+                return this.entries;
+            }
+
+            set
+            {
+                this.entries = value;
+            }
+        }
+
+        public class FuelEntry
+        {
+            public DateTime Date { get; set; }
+            public decimal Cost { get; set; }
+            public decimal Gallons { get; set; }
+        }
+    }
+}
