d470b5d6e86577e895920f1ada916a48ceb126bc
John Gietzen <otac0n@gietzen.us>
2011-06-07 20:54:24 -0500

    Added a database class.

---
+++ MyLife/Database.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Database.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife
+{
+    using System;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+
+    public class Database
+    {
+        private Database()
+        {
+        }
+
+        public static void Save()
+        {
+        }
+
+        public static Database Load()
+        {
+            return new Database();
+        }
+    }
+}
