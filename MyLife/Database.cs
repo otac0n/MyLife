e4488371fbbf47032f298c36ce9eb810431c706c
John Gietzen <otac0n@gietzen.us>
2011-06-08 19:25:41 -0500

    Moved `DataList<T>` into it's own file.

--- MyLife/Database.cs
+++ MyLife/Database.cs
@@ -9,9 +9,6 @@ namespace MyLife
     using System.IO;
     using System.IO.IsolatedStorage;
     using System.Runtime.Serialization;
-    using System.Collections.Generic;
-    using System.Collections.Specialized;
-    using System.Collections;
 
     public partial class Database
     {
@@ -56,114 +53,6 @@ namespace MyLife
                 using (var file = storage.OpenFile(DatabaseFileName, FileMode.Open))
                 {
                     return (Database)serializer.ReadObject(file);
-                }
-            }
-        }
-
-        public class DataList<T> : IList<T>, INotifyCollectionChanged
-        {
-            private readonly List<T> backingList = new List<T>();
-
-            public int IndexOf(T item)
-            {
-                return this.backingList.IndexOf(item);
-            }
-
-            public void Insert(int index, T item)
-            {
-                this.backingList.Insert(index, item);
-                this.RaiseCollectionChanged(
-                    () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
-            }
-
-            public void RemoveAt(int index)
-            {
-                var oldItem = this.backingList[index];
-                this.backingList.RemoveAt(index);
-                this.RaiseCollectionChanged(
-                    () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItem, index));
-            }
-
-            public T this[int index]
-            {
-                get
-                {
-                    return this.backingList[index];
-                }
-                set
-                {
-                    var oldItem = this.backingList[index];
-                    this.backingList[index] = value;
-                    this.RaiseCollectionChanged(
-                        () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, oldItem, index));
-                }
-            }
-
-            public void Add(T item)
-            {
-                var newIndex = this.backingList.Count;
-                this.backingList.Add(item);
-                this.RaiseCollectionChanged(
-                    () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, newIndex));
-            }
-
-            public void Clear()
-            {
-                this.backingList.Clear();
-                this.RaiseCollectionChanged(
-                    () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-            }
-
-            public bool Contains(T item)
-            {
-                return this.backingList.Contains(item);
-            }
-
-            public void CopyTo(T[] array, int arrayIndex)
-            {
-                this.backingList.CopyTo(array, arrayIndex);
-            }
-
-            public int Count
-            {
-                get { return this.backingList.Count; }
-            }
-
-            public bool IsReadOnly
-            {
-                get { return false; }
-            }
-
-            public bool Remove(T item)
-            {
-                var index = this.backingList.IndexOf(item);
-                if (index != -1)
-                {
-                    this.RemoveAt(index);
-                    return true;
-                }
-
-                return false;
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return this.backingList.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return ((IEnumerable)this.backingList).GetEnumerator();
-            }
-
-            public event NotifyCollectionChangedEventHandler CollectionChanged;
-
-            private void RaiseCollectionChanged(Func<NotifyCollectionChangedEventArgs> e)
-            {
-                var c = this.CollectionChanged;
-                if (c != null)
-                {
-                    c(this, e());
                 }
             }
         }
