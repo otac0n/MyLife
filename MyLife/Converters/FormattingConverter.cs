80afc222bc4f17cb60fca50b1562b5aa687fe5bd
John Gietzen <otac0n@gietzen.us>
2011-06-08 22:56:04 -0500

    Formatted the fuel entries list.

---
+++ MyLife/Converters/FormattingConverter.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="FormattingConverter.cs" company="(none)">
+//  Copyright © 2010 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+
+namespace MyLife.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class FormattingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var format = parameter == null ? null : parameter.ToString();
+
+            if (string.IsNullOrEmpty(format))
+            {
+                return value.ToString();
+            }
+
+            return string.Format(culture, format, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
